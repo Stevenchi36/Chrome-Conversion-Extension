9b16b893e1ef2ae3df18c65f29f6d65034946c2f
Steven Childrey <steven.childrey36@yahoo.com>
2016-10-01 00:46:17 -0400

    Update javascript for updateKG and adds ID to html

--- popup.js
+++ popup.js
@@ -1,22 +1,5 @@
-function message(val) {
-  $(".message").text(val);
+function updateKG() {
+  var x = getElementById('lb').value;
+  var y = 2.2046226218 * x;
+  document.getElementById("kg").value = y;
 }
-//Add something to test GitHub
-//set input to previously saved value
-chrome.storage.local.get("definedURL", function(result){
-    if(result.definedURL!=undefined){
-      $(".urlinput").attr("placeholder",result.definedURL);
-    }else{
-      $(".urlinput").attr("placeholder","type url here...");
-    }
-});
-
-//handle update
-  $(".urlbutton").click(function(){
-    $(".message").text($(".urlinput").val());
-    chrome.storage.local.set({'definedURL': $(".urlinput").val()}, function() {
-          // Notify that we saved.
-          message('Settings saved');
-          //google tracking here
-        });
-   });
