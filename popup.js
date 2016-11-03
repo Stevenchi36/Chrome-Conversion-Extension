4279f148886638bfa387e93f092ab434a99fabf4
Steven Childrey <steven.childrey36@yahoo.com>
2016-11-02 20:07:03 -0400

    Finally got it working!

--- popup.js
+++ popup.js
@@ -1,4 +1,17 @@
-function updateKG(val) {
-  var y = 2.2046226218 * val;
-  document.getElementById("kg").value = y;
+var translate = {
+	kgFromlb: function(){
+		document.getElementById('kg').value = parseFloat((document.getElementById("lb").value / 2.2046226218).toFixed(5));
+	},
+	lbFromkg: function(){
+		document.getElementById('lb').value = parseFloat((document.getElementById("kg").value * 2.2046226218).toFixed(5));
+	}
 }
+
+document.addEventListener('DOMContentLoaded',
+	function (){
+		var lb = document.getElementById('lb');
+		lb.addEventListener("keyup", function(){translate.kgFromlb();}, false);
+		var kg = document.getElementById('kg');
+		kg.addEventListener("keyup", function(){translate.lbFromkg();}, false);
+	}
+);
